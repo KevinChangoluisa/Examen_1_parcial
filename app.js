7e33417a64b5c0dd1b01a009fbc61673a5a0a294
KevinChangoluisa <kchangoluisa@est.ups.edu.ec>
2020-05-29 09:42:04 -0500

    Funcionalidad mostrar

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const argv = require('./config/yargs').argv;
+const tareas = require('./buscador/buscar');
 const colors = require('colors');
 let comando = argv._[0];
 
 
+
 switch (comando) {
     case 'mostrar':
-        let consulta = tareas.imprimir(argv.archivo, argv.anio, argv.pais);
+        let consulta = tareas.consulta(argv.archivo, argv.anio, argv.pais);
         break;
     case 'guardar':
         let guardar = tareas.guardar(argv.archivo, argv.anio, argv.pais);
---
+++ buscador/buscar.js
@@ -0,0 +1,86 @@
+const csv = require("csvtojson");
+const colors = require('colors');
+const fs = require('fs')
+
+async function getData(archivo) {
+    try {
+        const datos = await csv().fromFile(archivo);
+        let data = []
+        var codes = JSON.parse(fs.readFileSync('./buscador/ISO-3166-ALPHA-3.json', 'utf8'));
+        for (let dat of datos) {
+            dat = Object.values(dat);
+            for (let cod of codes) {
+                if (dat[1] == cod.countryCode) {
+                    data.push(dat);
+                }
+            }
+        }
+        return data;
+    } catch (error) {
+        error = "Error 200"
+        return error;
+    }
+}
+async function getCountry(pais, cod) {
+    for (var i = 0; i < pais.length; i++) {
+        let va = Object.values(pais[i]);
+        if (va[1] == cod) {
+            return true;
+        }
+    }
+}
+
+async function getSuscrip(pais, cod, anio) {
+    for (var i = 0; i < pais.length; i++) {
+        let va = Object.values(pais[i]);
+        if (va[1] == cod) {
+            suscrip = va[anio - 1956]
+            return suscrip
+        }
+    }
+}
+
+async function getPais(pais, cod) {
+    for (var i = 0; i < pais.length; i++) {
+        let va = Object.values(pais[i]);
+        if (va[1] == cod) {
+            namePais = va[0]
+            return namePais
+        }
+    }
+}
+
+
+
+async function consulta(archivo, anio, cod) {
+    let informacion = [];
+    let datos = await getData(archivo);
+    if (datos != "Error 200") {
+        let Country = await getCountry(datos, cod);
+        if (Country == true) {
+            let pais = await getPais(datos, cod);
+            if (anio >= 1964 && anio <= 2019) {
+                getSuscrip(datos, cod, anio)
+                    .then((suscriPais) => {
+                        console.log(`Pais:             ${pais}`);
+                        console.log(`Codigo pais:      ${cod}`);
+                        console.log(`Año:              ${anio}`);
+                        console.log(`Valor:            ${suscriPais}`);
+
+                    })
+            } else {
+                console.log('\n     ' + `Al momento no existe registros para el año: ${anio} `.bgRed);
+            }
+        } else {
+            console.log('\n     ' + `No existe el codigo de pais: ${cod} en la base de datos. `.bgRed);
+        }
+
+    } else {
+        console.log(`\n `, `::::No existe el archivo ${archivo} !::::`.bgRed)
+    }
+    return await informacion;
+}
+
+module.exports = {
+    consulta
+}
